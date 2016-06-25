484a2470e3c0e5f7b9deedf54c3d6bc47baba497
ERaBala <user@rimm03.local>
2016-06-25 13:55:02 +0530

    Initial Commitment using Swift - LineChart

--- SavioChart/ViewController.swift
+++ SavioChart/ViewController.swift
@@ -8,18 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// source - https://github.com/zemirco/swift-linechart
 
+class ViewController: UIViewController, LineChartDelegate {
+
+    var lineChart: LineChart!
+    var label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var views: [String: AnyObject] = [:]
+        
+        label.text = "Current Value"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(label)
+        views["label"] = label
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[label]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-80-[label]", options: [], metrics: nil, views: views))
+        
+        //   var data: [CGFloat] = [50, 30, 50, 113, 317, 50, 24,]
+        let data: [CGFloat] = [10, 30, 50, 113, 117, 50, 24]
+        
+        // simple line with custom x axis labels // hear need to pass json value
+        let xLabels: [String] = ["1'st","2nd", "3rd", "4th", "5th", "6th", "7th"]
+        
+        lineChart = LineChart()
+        lineChart.animation.enabled = true
+        lineChart.area = true
+        
+        // hide grid line Visiblity
+        lineChart.x.grid.visible = false
+        lineChart.y.grid.visible = false
+        
+        // hide dots visiblety in line chart
+       // lineChart.dots.visible = false
+        
+        lineChart.x.labels.visible = true
+        lineChart.x.grid.count = CGFloat(data.count)
+        lineChart.y.grid.count = CGFloat(xLabels.count)
+        lineChart.x.labels.values = xLabels
+        lineChart.y.labels.visible = true
+        
+        lineChart.dots.color = UIColor.blackColor()
+        
+        lineChart.addLine(data)
+        //  lineChart.addLine(data2)
+        
+        lineChart.translatesAutoresizingMaskIntoConstraints = false
+        lineChart.delegate = self
+        self.view.addSubview(lineChart)
+        views["chart"] = lineChart
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-[chart]-|", options: [], metrics: nil, views: views))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[label]-[chart(==200)]", options: [], metrics: nil, views: views))
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func didSelectDataPoint(x: CGFloat, yValues: Array<CGFloat>) {
+        label.text = "Value : \(yValues)"
     }
-
+    
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        if let chart = lineChart {
+            chart.setNeedsDisplay()
+        }
+    }
 
 }
 
